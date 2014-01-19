192e662d3af823d338e289b461c5da74df314411
Christine Emrich <christine.emrich@web.de>
2014-01-19 12:29:35 +0100

    Added compass

---
+++ js/compass.js
@@ -0,0 +1,26 @@
+define(function (require, exports, module) {
+
+	exports.isSupported = function () {
+		return DeviceOrientationEvent;
+	};
+
+	var result = document.querySelector('#compass .result');
+	var compass = document.querySelector('#compass .compass');
+
+	function onOrientation(event) {
+		var angle = event.alpha;
+		result.innerHTML = Math.round(angle) + '°';
+		compass.style.transform = compass.style.webkitTransform =
+			'rotate(' + angle + 'deg)';
+	}
+
+	// COMPASS
+	exports.onStartClick = function () {
+		window.addEventListener('deviceorientation', onOrientation, false);
+	};
+
+	exports.onStopClick = function () {
+		window.removeEventListener('deviceorientation', onOrientation, false);
+	};
+
+});
