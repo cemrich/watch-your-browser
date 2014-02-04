b4e12459d615c1198fd1a7153ec12ca149dc75df
Christine Emrich <christine.emrich@web.de>
2014-02-04 21:36:16 +0100

    added example for drag and drop api

---
+++ js/drag-and-drop.js
@@ -0,0 +1,75 @@
+define(function (require, exports, module) {
+
+	var dropNode = document.getElementById('drag-and-drop');
+	var output = document.querySelector('#drag-and-drop .output');
+
+	exports.isSupported = function () {
+		// very rough solution:
+		return 'draggable' in document.body;
+	};
+
+
+	// DRAG & DROP
+
+	function onDragEnter(event) {
+		dropNode.classList.add('over');
+	}
+
+	function onDragEnd(event) {
+		dropNode.classList.remove('over');
+	}
+
+	function onDragLeave(event) {
+		dropNode.classList.remove('over');
+	}
+
+	function onDragOver(event) {
+		if (event.preventDefault) {
+			event.preventDefault(); // necessary for dropping things
+		}
+		dropNode.classList.add('over');
+	}
+
+	function onDrop(event) {
+		dropNode.classList.remove('over');
+
+		// stops the browser from redirecting.
+		event.stopPropagation();
+		event.preventDefault();
+
+		var item;
+		var files = event.dataTransfer.files;
+		var types = event.dataTransfer.types;
+
+		output.innerHTML = '';
+
+		for (var i in types) {
+			var data = event.dataTransfer.getData(types[i]);
+			if (data) {
+				item = '<li>';
+				item += '<span>' + types[i] + '</span>';
+				item += data + '</li>';
+				output.innerHTML += item;
+			}
+		}
+
+		for (var i = 0; i < files.length; i++) {
+			file = files.item(i);
+			item = '<li><span>file';
+			if (file.type) {
+				item += ' (' + file.type + ')';
+			}
+			item += '</span>';
+			item += file.name + '<br>' + file.lastModifiedDate;
+			item += '</li>';
+			output.innerHTML += item;
+		}
+	}
+
+	dropNode.addEventListener('dragenter', onDragEnter, false);
+	dropNode.addEventListener('dragleave', onDragLeave, false);
+	dropNode.addEventListener('dragover', onDragOver, false);
+	dropNode.addEventListener('dragend', onDragEnd, false);
+	dropNode.addEventListener('drop', onDrop, false);
+
+});
