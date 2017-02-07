eacfd41b011286b6e625c683af27ea2d623059a9
Christine Emrich <christine.emrich@web.de>
2017-02-07 17:08:31 +0100

    Change battery demo to new api

--- js/battery.js
+++ js/battery.js
@@ -1,21 +1,23 @@
 define(function (require, exports, module) {
 
 	exports.isSupported = function () {
-		return 'battery' in navigator;
+		return 'getBattery' in navigator;
 	};
 
 	var battery = document.querySelector('#battery .indicator .inner');
 	var dischargedTime = document.querySelector('#battery .dischargedTime');
 	var chargedTime = document.querySelector('#battery .chargedTime');
 
+	var batteryManager = null;
+
 	function updateStatus() {
-		var height = navigator.battery.level * 100;
+		var height = batteryManager.level * 100;
 		battery.style.height = height + 'px';
 		battery.style.top = 100 - height + 'px';
 		battery.innerHTML = height + '%';
-		dischargedTime.innerHTML = navigator.battery.dischargingTime;
-		chargedTime.innerHTML = navigator.battery.chargingTime;
-		if (navigator.battery.charging) {
+		dischargedTime.innerHTML = batteryManager.dischargingTime;
+		chargedTime.innerHTML = batteryManager.chargingTime;
+		if (batteryManager.charging) {
 			battery.style.backgroundColor = '#fff';
 		} else {
 			battery.style.backgroundColor = '#2385BC';
@@ -24,11 +26,14 @@ define(function (require, exports, module) {
 
 	// BATTERY
 	if (exports.isSupported()) {
-		navigator.battery.addEventListener('chargingchange', updateStatus);
-		navigator.battery.addEventListener('levelchange', updateStatus);
-		navigator.battery.addEventListener('chargingtimechange', updateStatus);
-		updateStatus();
-
+		navigator.getBattery().then(function(battery) {
+			batteryManager = battery;
+			batteryManager.addEventListener('chargingchange', updateStatus);
+			batteryManager.addEventListener('levelchange', updateStatus);
+			batteryManager.addEventListener('chargingtimechange', updateStatus);
+			batteryManager.addEventListener('dischargingtimechange', updateStatus);
+			updateStatus();
+		});
 	}
 
 });
