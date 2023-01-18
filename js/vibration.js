ec9286325d2a9197ee94222e34f81f0cb14e737e
Christine Coenen <code.coenen@gmail.com>
2023-01-18 18:59:26 +0100

    Remove vendor prefixes from vibration

--- js/vibration.js
+++ js/vibration.js
@@ -4,13 +4,6 @@ define(function (require, exports, module) {
 		return 'vibrate' in navigator;
 	};
 
-	// VIBRATE
-	navigator.vibrate =
-		navigator.vibrate ||
-		navigator.webkitVibrate ||
-		navigator.mozVibrate ||
-		navigator.msVibrate;
-
 	exports.onTryClick = function() {
 		navigator.vibrate([200, 40, 40, 40, 20, 40, 100]);
 	};
